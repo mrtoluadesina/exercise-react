22cc74bb9f745a1b1bfc64be75bd15f23e201dc8
Tolu Adesina <tadesina90@gmail.com>
2019-06-28 16:19:41 +0100

    Driver component updated and displays all content when clicked

--- client/src/pages/Driver.js
+++ client/src/pages/Driver.js
@@ -2,6 +2,31 @@ import React from 'react';
 import { Query } from 'react-apollo';
 import { GET_DRIVER_DETAILS } from '../query';
 
+import male from '../male.svg';
+import female from '../female.svg';
+
+import styled from 'styled-components';
+
+const ContentArea = styled.div`
+  display: flex;
+  flex: 0 0 80%;
+`;
+
+const List = styled.li`
+  display: flex;
+  justify-content: space-between;
+  font-size: 2rem;
+  line-height: 3rem;
+  padding-bottom: 1rem;
+  margin-bottom: 2rem;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+`;
+
+const Section = styled.section`
+  display: flex;
+  padding: 10%;
+`;
+
 export default function Driver() {
   const driverID = window.location.pathname.replace('/driver/', '');
   return (
@@ -14,9 +39,64 @@ export default function Driver() {
         loading ? (
           <span>I am loading ...</span>
         ) : (
-          <div className="content-area">
-            {data.driver ? data.driver.name : ''}
-          </div>
+          <ContentArea>
+            {data.driver ? (
+              <>
+                <div className="half">
+                  <img
+                    className="avatar"
+                    src={data.driver.gender === 'male' ? male : female}
+                    alt="driver icon"
+                  />
+                  <ul>
+                    <List>
+                      <span>NAME:</span> <span>{data.driver.name}</span>
+                    </List>
+                    <List>
+                      <span>D.O.B:</span>{' '}
+                      <span>{data.driver.dob.replace(/T.+$/, '')}</span>
+                    </List>
+                    <List>
+                      <span>GENDER:</span> <span>{data.driver.gender}</span>
+                    </List>
+                    <List>
+                      <span>EMAIL:</span> <span>{data.driver.email}</span>
+                    </List>
+                    <List>
+                      <span>PHONE:</span> <span>{data.driver.phone}</span>
+                    </List>
+                  </ul>
+                </div>
+                <div className="half">
+                  <div className="half-half">
+                    <ul>
+                      <List>
+                        <span>AGENT:</span> <span>{data.driver.agent}</span>
+                      </List>
+                      <List>
+                        <span>ADDRESS:</span> <span>{data.driver.address}</span>
+                      </List>
+                    </ul>
+                  </div>
+                  <div className="half-half">
+                    <ul>
+                      {data.driver.vehicles.map((vehicle, index) => (
+                        <List>
+                          <span>{index + 1}</span>{' '}
+                          <span>{vehicle.manufacturer}</span>{' '}
+                          <span>{vehicle.plate}</span>
+                        </List>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <Section>
+                <h2>Click a Driver on the left</h2>
+              </Section>
+            )}
+          </ContentArea>
         )
       }
     </Query>
