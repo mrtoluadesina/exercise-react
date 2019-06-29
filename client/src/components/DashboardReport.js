5a5b83f3b3d44b87a1a4e1886a0f2f81b4f5e6c3
Tolu Adesina <tadesina90@gmail.com>
2019-06-29 20:48:16 +0100

    Dashboard design recreated

--- client/src/components/DashboardReport.js
+++ client/src/components/DashboardReport.js
@@ -7,6 +7,12 @@ import './components.css';
 import { gender, cashOrNot, totals } from '../services/chartWorker';
 import Tabled from './Table';
 
+import styled from 'styled-components';
+
+const Section = styled.section`
+  display: flex;
+`;
+
 export default function DashboardReport() {
   return (
     <Query query={TRIPREPORT} errorPolicy="all">
@@ -15,13 +21,15 @@ export default function DashboardReport() {
           <span>I am loading ...</span>
         ) : (
           <>
-            <div className="pies">
-              <PieChart data={gender(data.trips)} />
-              <PieChart data={cashOrNot(data.trips)} />
-            </div>
-            <div className="bars">
-              <ColumnChart data={totals(data.trips)} />
-            </div>
+            <Section>
+              <div className="pies">
+                <PieChart data={gender(data.trips)} />
+                <PieChart data={cashOrNot(data.trips)} />
+              </div>
+              <div className="bars">
+                <ColumnChart data={totals(data.trips)} />
+              </div>
+            </Section>
             <Tabled data={data.trips} />
           </>
         )
